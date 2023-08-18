4aace3c5cf69cbeea5c746143b13fb0fb98be8c0
alkaz07 <>
2023-08-18 10:12:45 +0300

    ...

--- src/main/java/com/example/kodirovka/HelloController.java
+++ src/main/java/com/example/kodirovka/HelloController.java
@@ -16,47 +16,54 @@ public class HelloController {
     @FXML
     Button button;
 
-        public List<String> doResult() throws FileNotFoundException {
-            List<String> decrypted=vvodTeksta();
-            Map<String,String> slovar=slovar();
-            for (int i = 0; i < decrypted.size(); i++) {
-                String word=decrypted.get(i);
-                if(slovar.containsKey(word)){
-                    String replace=slovar.get(word);
-                    decrypted.set(i,replace);
-                }
-            }
-            result.setText(decrypted.toString());
-            return decrypted;
-        }
+    @FXML
+    public void doResult() throws FileNotFoundException {
+        List<String> decrypted = vvodTeksta();
+        Map<String, String> slovar = slovar();
+        decrypted = encrypt(decrypted, slovar);
 
-        public List<String> vvodTeksta() throws FileNotFoundException {
-            File file = new File("1.txt");
-            Scanner scanner = new Scanner(file);
-            ArrayList<String> arrayList = new ArrayList<>();
-            while (scanner.hasNextLine()) {
-                arrayList.add(scanner.nextLine());
-            }
-            vvod.setText(arrayList.toString());
-            return arrayList;
-        }
-
-        public Map<String, String> slovar() throws FileNotFoundException {
-            File file = new File("2.txt");
-            Scanner scanner = new Scanner(file);
-            Map<String, String> map = new HashMap<>();
-            while (scanner.hasNextLine()) {
-                String[] columns=scanner.nextLine().split("\s+");
-                if (columns.length>=2){
-                    String key=columns[0];
-                    String value=columns[1];
-                    map.put(key,value);
-                }
-            }
-
-            return map;
-        }
+        result.setText(decrypted.toString());
     }
+
+    public List<String> encrypt(List<String> text, Map<String, String> slovar) {
+        for (int i = 0; i < text.size(); i++) {
+            //todo пока что в переменной word целая большая фраза. Надо разбивать на слова
+            String word = text.get(i);
+            if (slovar.containsKey(word)) {
+                String replace = slovar.get(word);
+                text.set(i, replace);
+            }
+        }
+        return text;
+    }
+
+    public List<String> vvodTeksta() throws FileNotFoundException {
+        File file = new File("1.txt");
+        Scanner scanner = new Scanner(file);
+        ArrayList<String> arrayList = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            arrayList.add(scanner.nextLine());
+        }
+        vvod.setText(arrayList.toString());
+        return arrayList;
+    }
+
+    public Map<String, String> slovar() throws FileNotFoundException {
+        File file = new File("2.txt");
+        Scanner scanner = new Scanner(file);
+        Map<String, String> map = new HashMap<>();
+        while (scanner.hasNextLine()) {
+            String[] columns = scanner.nextLine().split("\s+");
+            if (columns.length >= 2) {
+                String key = columns[0];
+                String value = columns[1];
+                map.put(key, value);
+            }
+        }
+
+        return map;
+    }
+}
 
 
 
